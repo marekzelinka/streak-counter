339793a7ea0896635aa00388d442c272362c3882
Marek Zelinka <mzelinka17@gmail.com>
2024-08-19 19:10:40 +0200

    get streak if it already exists

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
+import { STREAK_KEY } from './lib'
 import type { Streak } from './types'
 import { formatDate } from './utils'
 
 export function streakCounter(localStorage_: Storage, date: Date): Streak {
-  return {
+  const rawStreak = localStorage_.getItem(STREAK_KEY)
+  if (rawStreak) {
+    try {
+      const streak = JSON.parse(rawStreak) as Streak
+
+      return streak
+    } catch (error) {
+      console.error('Failed to parse streak from localStorage')
+    }
+  }
+
+  const streak: Streak = {
     currentCount: 1,
     startDate: formatDate(date),
     lastLoginDate: formatDate(date),
   }
+
+  localStorage_.setItem(STREAK_KEY, JSON.stringify(streak))
+
+  return streak
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,8 @@
 import { JSDOM } from 'jsdom'
 import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import { streakCounter } from '../src'
+import { STREAK_KEY } from '../src/lib'
+import type { Streak } from '../src/types'
 import { formatDate } from '../src/utils'
 
 describe('streakCounter', () => {
@@ -29,5 +31,44 @@ describe('streakCounter', () => {
     expect(streak.currentCount).toBe(1)
     const formattedDate = formatDate(date)
     expect(streak.lastLoginDate).toBe(formattedDate)
+  })
+
+  it('should store the streak in localStorage', () => {
+    const date = new Date()
+    streakCounter(mockLocalStorage, date)
+
+    const rawStreak = mockLocalStorage.getItem(STREAK_KEY)
+    expect(rawStreak).not.toBeNull()
+  })
+
+  describe('with a pre-populated streak', () => {
+    let mockLocalStorage: Storage
+
+    beforeEach(() => {
+      const mockJSDOM = new JSDOM('', { url: 'https://localhost' })
+      mockLocalStorage = mockJSDOM.window.localStorage
+
+      // Use a date in the past so it is always the same
+      const startDate = new Date('12/12/2021')
+      // We set the currentCount to 2 so the lastLoginDate must be one day ahead
+      const lastLoginDate = new Date('12/13/2021')
+      const streak: Streak = {
+        currentCount: 2,
+        startDate: formatDate(startDate),
+        lastLoginDate: formatDate(lastLoginDate),
+      }
+      mockLocalStorage.setItem(STREAK_KEY, JSON.stringify(streak))
+    })
+
+    afterEach(() => {
+      mockLocalStorage.clear()
+    })
+
+    it('should return the streak from localStorage', () => {
+      const date = new Date()
+      const streak = streakCounter(mockLocalStorage, date)
+      // Should match the dates used to set up the test
+      expect(streak.startDate).toBe('12/12/2021')
+    })
   })
 })
